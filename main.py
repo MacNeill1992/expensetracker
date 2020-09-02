36c2cbf1c75e69e5c0df043feefb6ba1b973d7f5
Michael1992 <michaelmacneill@live.ca>
2020-09-01 23:18:53 -0400

    building it back up-007

--- main.py
+++ main.py
@@ -48,14 +48,6 @@ class ImageButton(ButtonBehavior, Image):
 class LabelButton(ButtonBehavior, Label):
     pass
 
-#########DEL###########
-class Screen1(Screen):
-    pass
-class Screen2(Screen):
-    pass
-#########DEL############
-
-
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
     user_id = 1
@@ -67,30 +59,22 @@ class MainApp(App):
     def build(self):
         self.my_firebase = MyFirebase()
         return GUI
-#######UP TO DATE ^^^^######
+
     def on_start(self):
         try:
-            with open("refresh_token.txt", 'r') as f:
-                refresh_token = f.read()
-            id_token, local_id = self.my_firebase.exchange_refresh_token(refresh_token)
-            self.local_id = local_id
-            self.id_token = id_token
-            # Get database data
-            result = requests.get("https://expensetracker-d3a98.firebaseio.com/" + local_id + ".json?auth=" + id_token)
-            data = json.loads(result.content.decode())
-
-            self.change_screen("connected")
+            self.get_data()
+            # populate add expense category srollview
+            self.add_expense_icons()
+            # initial screen load of expenses by month on startup
+            self.expense_by_month()
+            #logged in
+            self.on_log_in("connected", NoTransition())
 
         except Exception:
             print("EXCEPTION")
             traceback.print_exc()
             print("EXCEPTION")
             pass
-
-
-
-
-
 
     def get_data(self):
         with open("refresh_token.txt", 'r') as f:
@@ -353,17 +337,4 @@ class MainApp(App):
         self.root.ids.login_screen.ids.login_password.text = ""
 
 
-###################DEL###################3
-    def change_screen(self, screen_name):
-        screen_manager = self.root.ids['screen_manager']
-        screen_manager.current = screen_name
-
-    def connected_change_screen(self, screen_name):
-        screen_manager = self.root.ids['connected'].ids['connected_screen_manager']
-        screen_manager.current = screen_name
-##################DEL####################
-
-
-if __name__ == "main":
-    MainApp().run()
-
+MainApp().run()
