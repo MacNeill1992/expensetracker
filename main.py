542aeb4611e737590b4ac858fc269248df31c9f7
Michael1992 <michaelmacneill@live.ca>
2020-09-01 23:01:58 -0400

    building it back up-003

--- main.py
+++ main.py
@@ -1,30 +1,63 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import SlideTransition, NoTransition
+from kivy.uix.button import ButtonBehavior
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.graphics import Color, RoundedRectangle, Ellipse, Line
+import kivy.utils
+from expense_by_month import ExpenseByMonth
+from expense_by_category import ExpenseByCategory
 from kivy.config import Config
+from kivy.properties import StringProperty
 from myfirebase import MyFirebase
-import json
 import traceback
+from os import walk
+# Config.set('graphics', 'width', '260')
+# Config.set('graphics', 'height', '480')
 import requests
-# Config.set('graphics', 'width', '500')
-# Config.set('graphics', 'height', '850')
+import json
+from functools import partial
+from datetime import datetime
+import certifi
 
 class LoginScreen(Screen):
     pass
 
-class Screen1(Screen):
-    pass
-
-class Screen2(Screen):
+class AddExpenseScreen(Screen):
     pass
 
 class Connected(Screen):
     pass
 
+class ExpenseScreen(Screen):
+    pass
+
+class CategoryScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+class ImageButton(ButtonBehavior, Image):
+    pass
+
+class LabelButton(ButtonBehavior, Label):
+    pass
+
+#########DEL###########
+class Screen1(Screen):
+    pass
+class Screen2(Screen):
+    pass
+#########DEL############
+
 
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
-
     user_id = 1
     category_image = None
     category_image_widget = ''
@@ -34,9 +67,8 @@ class MainApp(App):
     def build(self):
         self.my_firebase = MyFirebase()
         return GUI
-
+#######UP TO DATE ^^^^######
     def on_start(self):
-
         try:
             with open("refresh_token.txt", 'r') as f:
                 refresh_token = f.read()
